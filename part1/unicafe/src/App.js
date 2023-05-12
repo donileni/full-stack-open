d4b7ba80292e196ad9655889f800db60e4f164f2
David Olsson <david.markus.olsson@gmail.com>
2023-05-12 14:45:53 +0200

    Finished 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -23,17 +23,49 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const handleGoodClick = () => {
+    const updatedAll = all + 1
+    setAll(updatedAll)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setAverage((updatedGood - bad)/updatedAll)
+    setPositive(updatedGood/updatedAll)
+  }
+
+  const handleBadClick = () => {
+    const updatedAll = all + 1
+    setAll(updatedAll)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setAverage((good - updatedBad)/updatedAll)
+    setPositive(good/updatedAll)
+  }
+
+  const handleNeutralClick = () => {
+    const updatedAll = all + 1
+    setAll(updatedAll)
+    const newNeutral = neutral + 1
+    setNeutral(newNeutral)
+    setPositive(good/updatedAll)
+  }
 
   return (
     <div>
       <Header text="give feedback"/>
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={() => handleGoodClick()} text="good" />
+      <Button handleClick={() => handleNeutralClick()} text="neutral" />
+      <Button handleClick={() => handleBadClick()} text="bad" />
       <Header text="statistics"/>
       <Display text="good" value={good} />
       <Display text="neutral" value={neutral} />
       <Display text="bad" value={bad} />
+      <Display text="all" value={all} />
+      <Display text="average" value={average} />
+      <Display text="positive" value={positive} />
     </div>
   )
 }
