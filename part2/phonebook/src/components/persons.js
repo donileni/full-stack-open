c0a94e218b5da227a005e0192271aa045972d7d4
David Olsson <david.markus.olsson@gmail.com>
2023-06-25 20:02:18 +0200

    Finished 2.10

---
+++ part2/phonebook/src/components/persons.js
@@ -0,0 +1,10 @@
+const Persons = (props) => {
+
+    return (
+        <div>
+            {props.personsToShow.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+        </div>
+    )
+}
+
+export default Persons
