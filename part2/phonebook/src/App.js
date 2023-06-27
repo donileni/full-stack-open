561a2eb61302be42b0190cd6cf7722060a3cf748
David Olsson <david.markus.olsson@gmail.com>
2023-06-27 13:12:49 +0200

    Finished 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from "./components/filter.js"
 import PersonForm from "./components/person-form.js"
 import Persons from "./components/persons.js"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
 
   const handleClick = (event) => {
     event.preventDefault()
