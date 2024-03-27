45d3cbd9476f9341519e28427af9c5ff416ccb1c
David <david.markus.olsson@gmail.com>
2024-03-27 18:40:54 +0100

    finished 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,20 @@
+const blog = require("../models/blog")
+
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes = (blogs) => {
+
+    if (blogs.length === 0) {
+        return 0
+    }
+
+    const allLikes = blogs.map(x => x.likes) 
+    const sum = allLikes.reduce((a,b) => a + b, 0)
+    return sum
+}
   
   module.exports = {
-    dummy
+    dummy, totalLikes
   }
