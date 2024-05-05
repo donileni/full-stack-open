a75e89a5f453600ad6eda268cf5a9e74fac582d8
David <david.markus.olsson@gmail.com>
2024-05-05 17:11:21 +0200

    Finished 4.8

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,17 +1,46 @@
-const { test, after } = require('node:test')
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
+const { test, after, beforeEach, describe } = require("node:test");
+const mongoose = require("mongoose");
+const Blog = require("../models/blog");
+const supertest = require("supertest");
+const app = require("../app");
+const assert = require("node:assert");
 
-const api = supertest(app)
+const api = supertest(app);
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+const initialBlogs = [
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+    likes: 5,
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+    likes: 20,
+  },
+];
 
-after(async () => {
-  await mongoose.connection.close()
-})
+describe("blog api", () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+    let blogObject = new Blog(initialBlogs[0]);
+    await blogObject.save();
+    blogObject = new Blog(initialBlogs[1]);
+    await blogObject.save();
+  });
+
+  after(async () => {
+    await mongoose.connection.close();
+  });
+
+  test("correct amount of blogs in JSON", async () => {
+    const response = await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    assert.strictEqual(response.body.length, initialBlogs.length);
+  });
+});
