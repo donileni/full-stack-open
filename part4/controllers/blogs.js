3201e87d6b5c772c1af193373fbcad2d55e797f4
David <david.markus.olsson@gmail.com>
2024-09-15 11:10:20 +0200

    Finished 4.19

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,11 +1,22 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require('../models/user')
+const jwt = require('jsonwebtoken');
+const { error } = require("../utils/logger");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
@@ -14,7 +25,12 @@ blogsRouter.post("/", async (request, response) => {
     return response.status(400).json({ "message": "Need to have title and URL" })
   }
 
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid'})
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
