e1407c76b6057723235799d0c8bc31f1598e09e6
David <david.markus.olsson@gmail.com>
2024-09-17 17:25:19 +0200

    Finished 4.22

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -40,8 +40,23 @@ blogsRouter.post("/", async (request, response) => {
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
-  response.status(204).end()
+  
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+
+  const user = await User.findById(decodedToken.id)
+
+  if (blog.user.toString() === user.id.toString()) {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } else {
+    response.status(401).json({ error: 'user can only delete own blogs'})
+  }
+
 })
 
 blogsRouter.put("/:id", async (request, response) =>{
