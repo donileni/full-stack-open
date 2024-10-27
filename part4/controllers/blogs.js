2577c10d90fe74b445cca72ada3702a1caba7c4d
David <david.markus.olsson@gmail.com>
2024-10-27 11:08:13 +0100

    added user when creating blog via form

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -57,7 +57,8 @@ blogsRouter.put("/:id", async (request, response) =>{
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: body.user
   }
 
   await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
