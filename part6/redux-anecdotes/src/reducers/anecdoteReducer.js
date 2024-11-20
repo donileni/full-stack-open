79ec889a854a3876f545a56868051a3a2e40df9a
David <david.markus.olsson@gmail.com>
2024-11-20 19:05:38 +0100

    Finished 6.3

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,23 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'VOTE': {
+      const id = action.payload.id
+      const anecdoteToChange = state.find(anecdote => anecdote.id === id)
+      const changedAnecdote = {...anecdoteToChange, votes: anecdoteToChange.votes + 1}
+      return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
+    }
+
+    default: return state
+  }
+}
+
+export const newVote = (id) => {
+  return {
+    type: 'VOTE',
+    payload: {id}
+  }
 }
 
 export default reducer
