7943a743eefa71091d1f1d7b334d6c4d720b8ee0
David <david.markus.olsson@gmail.com>
2024-11-25 20:14:06 +0100

    Finished 6.22

--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
+import anecdotes from '../../redux-anecdotes/src/services/anecdotes'
 
 const baseUrl = 'http://localhost:3001/anecdotes'
 
 export const getAll = () => axios.get(baseUrl).then(res => res.data)
 
 export const createAnecdote = (newAnecdote) => axios.post(baseUrl, newAnecdote).then(res => res.data)
+
+export const updatedAnecdote = anecdote => axios.put(`${baseUrl}/${anecdote.id}`, anecdote).then(res => res.data)
