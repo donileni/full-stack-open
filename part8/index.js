bc501083ba2b11c7718e0359d2ba0064af698aa5
David <david.markus.olsson@gmail.com>
2025-01-24 22:17:42 +0100

    Finished 8.4

--- part8/index.js
+++ part8/index.js
@@ -98,7 +98,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks (author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -107,13 +107,18 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+
+      const booksByAuthor = books.filter((book) => book.author === args.author)
+      return booksByAuthor
+    },
     allAuthors: () => authors,
   },
   Author: {
-    bookCount: (root) => {
-      return books.filter((book) => book.author === root.name).length;
-    },
+    bookCount: (root) => books.filter((book) => book.author === root.name).length,
   },
 };
 
