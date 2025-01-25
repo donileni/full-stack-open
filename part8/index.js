0f0ac741d178479fe6ede0a751c2cf98229640c3
David <david.markus.olsson@gmail.com>
2025-01-25 10:04:43 +0100

    Finished 8.7

--- part8/index.js
+++ part8/index.js
@@ -110,6 +110,10 @@ const typeDefs = `
       author: String!
       genres: [String!]!
     ) : Book
+     editAuthor(
+      name: String!
+      setBornTo: Int! 
+     ): Author
   }
 `;
 
@@ -151,6 +155,17 @@ const resolvers = {
       books = books.concat(book);
       return book;
     },
+    editAuthor: (root, args) => {
+      const authorToChange = authors.find((author) => author.name === args.name)
+
+      if (!authorToChange) {
+        return null
+      }
+
+      const updatedAuthor = { ...authorToChange, born: args.setBornTo}
+      authors = authors.map((author) => (author.name === args.name ? updatedAuthor: author))
+      return updatedAuthor
+    }
   },
 };
 
